3ae6068d91c6038699738751fd98a909da4e46b1
Saveli <oisurka@gmail.com>
2020-05-06 14:13:09 +0300

    feat: add 4, 5 tasks

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,63 @@
 package subtask4
 
+import kotlin.math.absoluteValue
+import kotlin.math.sqrt
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        if (number < 4) return null
+        var array = ArrayList<Int>() as MutableList<Int>
+        for (it in number - 1 downTo 1) {
+            array.add(it)
+            var diff = number * number - it * it
+            var i = 1
+            while (nearestQuad(diff) < array.last()) {
+                array.add(nearestQuad(diff))
+                diff -= array[i] * array[i]
+                i++
+            }
+
+            if (diff == 0) {
+                array.removeAt(array.lastIndex)
+                return array.reversed().toTypedArray()
+            } else {
+                var j = findItemToChange(array)
+                while (j != 0) {
+                    array[j] -= 1
+                    diff = number * number - array.take(j + 1).map { it2 -> it2 * it2 }.sum()
+                    array = array.filterIndexed { index, it2 -> index <= j } as MutableList<Int>
+                    if (diff < array.last() * array.last()) {
+                        var arrayOfCoeffs = findCoeffs(diff)
+                        array.addAll(arrayOfCoeffs)
+                        if (array.last() == 0) {
+                            array.remove(array.last())
+                            return array.reversed().toTypedArray()
+                        } else {
+                            array.remove(array.last())
+                        }
+                    }
+                    j = findItemToChange(array)
+                }
+            }
+        }
+        return array.reversed().toTypedArray()
     }
+
+    private fun findItemToChange(array: MutableList<Int>) = array.indexOfLast { it > 2 }
+
+    private fun findCoeffs(diff: Int): Collection<Int> {
+        var resultDiff = diff
+        val array = ArrayList<Int>()
+        array.add(nearestQuad(resultDiff))
+        resultDiff -= nearestQuad(resultDiff) * nearestQuad(resultDiff)
+        while (!(2..3).contains(resultDiff) && resultDiff != 0 && nearestQuad(resultDiff) < array.last()) {
+            array.add(nearestQuad(resultDiff))
+            resultDiff -= nearestQuad(resultDiff) * nearestQuad(resultDiff)
+        }
+        array.add(resultDiff)
+        return array
+    }
+
+    private fun nearestQuad(n: Int) = sqrt(n.toDouble()).absoluteValue.toInt()
 }
--- src/main/kotlin/subtask5/TelephoneFinder.kt
+++ src/main/kotlin/subtask5/TelephoneFinder.kt
@@ -1,11 +1,63 @@
 package subtask5
 
 import kotlin.reflect.KClass
+import java.lang.StringBuilder
 
 class TelephoneFinder {
 
-    // TODO: Complete the following function
     fun findAllNumbersFromGivenNumber(number: String): Array<String>? {
-        throw NotImplementedError("Not implemented")
+        if (number.first() == '-') return null
+        val array = ArrayList<String>()
+        var str = StringBuilder(number)
+        number.forEachIndexed { i, it ->
+            when (it) {
+                '0' -> array.add(str.replace(i, i + 1, "8").toString())
+                '1' -> {
+                    array.add(str.replace(i, i + 1, "2").toString())
+                    array.add(str.replace(i, i + 1, "4").toString())
+                }
+                '2' -> {
+                    array.add(str.replace(i, i + 1, "1").toString())
+                    array.add(str.replace(i, i + 1, "3").toString())
+                    array.add(str.replace(i, i + 1, "5").toString())
+                }
+                '3' -> {
+                    array.add(str.replace(i, i + 1, "2").toString())
+                    array.add(str.replace(i, i + 1, "6").toString())
+                }
+                '4' -> {
+                    array.add(str.replace(i, i + 1, "1").toString())
+                    array.add(str.replace(i, i + 1, "5").toString())
+                    array.add(str.replace(i, i + 1, "7").toString())
+                }
+                '5' -> {
+                    array.add(str.replace(i, i + 1, "2").toString())
+                    array.add(str.replace(i, i + 1, "4").toString())
+                    array.add(str.replace(i, i + 1, "6").toString())
+                    array.add(str.replace(i, i + 1, "8").toString())
+                }
+                '6' -> {
+                    array.add(str.replace(i, i + 1, "3").toString())
+                    array.add(str.replace(i, i + 1, "5").toString())
+                    array.add(str.replace(i, i + 1, "9").toString())
+                }
+                '7' -> {
+                    array.add(str.replace(i, i + 1, "4").toString())
+                    array.add(str.replace(i, i + 1, "8").toString())
+                }
+                '8' -> {
+                    array.add(str.replace(i, i + 1, "0").toString())
+                    array.add(str.replace(i, i + 1, "5").toString())
+                    array.add(str.replace(i, i + 1, "7").toString())
+                    array.add(str.replace(i, i + 1, "9").toString())
+                }
+                '9' -> {
+                    array.add(str.replace(i, i + 1, "6").toString())
+                    array.add(str.replace(i, i + 1, "8").toString())
+                }
+            }
+            str = StringBuilder(number)
+        }
+        return array.toTypedArray()
     }
 }
